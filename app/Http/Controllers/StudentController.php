21e3ad0654d4034cf55692d7091107d5829758d7
mohamadkaser <1700004580@stu.iku.edu.tr>
2021-06-05 11:58:59 +0300

    here i Created a controller function to save the Student information.

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function addStudent(){
+        return view('addStudent');
+    }
+
+    public function addStudentSubmit(Request $req){
+        DB::table('student')->insert([
+            'name' => $req->name,
+            'surname' => $req->surname,
+            'department' => $req->department
+        ]);
+
+        return back()->with('student_created', 'Student is created successful');
+    }
 }
